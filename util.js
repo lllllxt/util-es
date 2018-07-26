cd588c23d1a985f6b4bbad98e29612601664da7a
刘晓堂 <532543299@qq.com>
2018-07-26 12:02:12 +0800

    优化sessionStorage操作, 可设置过期时间

--- util.js
+++ util.js
@@ -103,19 +103,55 @@ function onlyNum(event, length) {
     }
 }
 
-function getSessionJson(key) {
-    isEmpty(sessionStorage.getItem(key)) ? sessionStorage.setItem(key, JSON.stringify({})) : '';
-    return JSON.parse(sessionStorage.getItem(key));
-}
+/**
+ * 获取sessionStorage
+ * @param key
+ * @returns {any}
+ */
+function getSession(key) {
+    let result = null;
+    let _session = JSON.parse(sessionStorage.getItem(key));
+    let isObj = Object.prototype.toString.call(_session) === '[object Object]';
+    if (isObj && _session.$$ExpiryTime) {
+        if ((new Date().getTime() > new Date(_session.$$ExpiryTime).getTime())) { // 过期
+            result = null;
+        } else {
+            result = _session.data;
+        }
+    } else {
+        result = _session;
+    }
+    return result;
+};
 
-function setSessionJson(key, json) {
-    sessionStorage.setItem(key, JSON.stringify(json));
-    return sessionStorage.getItem(key);
-}
+/**
+ * 设置sessionStorage (可存对象)
+ * @param key 键值
+ * @param dataSource 数据
+ * @param second 过期时间(s)
+ * @returns {any}
+ */
+function setSession(key, dataSource, second) {
+    let result = {};
+    if (second) {
+        const d = new Date();
+        d.setTime(d.getTime() + (second * 1000));
+        result.data = dataSource;
+        result.$$ExpiryTime = d;
+    } else {
+        result = dataSource;
+    }
+    sessionStorage.setItem(key, JSON.stringify(result));
+    return result;
+};
 
+/**
+ * 移除sessionStorage
+ * @param key
+ */
 function removeSession(key) {
-    if (!isEmpty(key)) sessionStorage.removeItem(key);
-}
+    key && sessionStorage.removeItem(key);
+};
 
 /**
  * [formatStr
