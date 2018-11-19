a07ed00b03d5dba1db6a81dc56181456ec57426f
刘晓堂 <532543299@qq.com>
2018-11-19 14:29:24 +0800

    Update uuid.js

--- src/uuid.js
+++ src/uuid.js
@@ -1,3 +1,6 @@
+/**
+ * 这是来自某网友的代码, 由于时间太久了,我也忘了是在哪找的了 所以没办法标注出处
+ */
 export default function () {
     const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
     const uuid = new Array(32);
